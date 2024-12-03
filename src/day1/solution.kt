61f17f0424f687f862ba1e672459b8828e28251b
Eder Lima <lima.eder101@gmail.com>
2024-12-02 22:32:38 -0300

    fix: remove list extension from source

--- src/day1/solution.kt
+++ src/day1/solution.kt
@@ -12,24 +12,3 @@ fun run() {
 
     println("Sum of distances is: $distances")
 }
-
-data class Tuple<T>(val first: T, val second: T)
-
-fun <T> List<T>.zip(other: List<T>): Iterable<Tuple<T>> {
-    val iterator1 = this.iterator()
-    val iterator2 = other.iterator()
-
-    return object : Iterable<Tuple<T>> {
-        override fun iterator(): Iterator<Tuple<T>> {
-            return object : Iterator<Tuple<T>> {
-                override fun hasNext(): Boolean {
-                    return iterator1.hasNext() && iterator2.hasNext()
-                }
-
-                override fun next(): Tuple<T> {
-                    return Tuple(iterator1.next(), iterator2.next())
-                }
-            }
-        }
-    }
-}
